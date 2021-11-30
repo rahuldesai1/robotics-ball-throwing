c548396fcd8344d471c6153d6ff68127a1b0ac34
Rahul Desai <rahuldesai@berkeley.edu>
2021-11-29 21:06:20 -0800

    Small updates to picker.py

--- src/control/src/picker.py
+++ src/control/src/picker.py
@@ -7,22 +7,26 @@ from moveit_msgs.srv import GetPositionIK, GetPositionIKRequest, GetPositionIKRe
 from control.srv import PickUpBall
 from geometry_msgs.msg import PoseStamped
 from moveit_commander import MoveGroupCommander
-from computer_vision.srv import GetBallPose
+# from computer_vision.srv import GetBallPose
 from baxter_interface import gripper as robot_gripper
 
 class Picker:
     def __init__(self):
         # Wait for the dependent services to become available
-        rospy.wait_for_service('ball_pose')
+        # rospy.wait_for_service('ball_pose')
 
-        #Initialize the node
+        # Initialize the node
         rospy.init_node('picker_listener')
-        #Create the service
-        rospy.Service('pick_ball', PickUpBall, self.pickBall)
+        # Create the service
+        rospy.Service('picker', PickUpBall, self.pickBall)
 
         # Create the function used to get the position of the ball
         # self.get_ball_pose = rospy.ServiceProxy('ball_pose', GetBallPose)
-        self.get_ball_pose = lambda: None
+        mocked_result = PoseStamped()
+        mocked_result.pose.position.x = 0.587
+        mocked_result.pose.position.y = -0.720
+        mocked_result.pose.position.z = 0.065
+        self.get_ball_pose = lambda: mocked_result
         self.compute_ik = rospy.ServiceProxy('compute_ik', GetPositionIK)
 
         self.arm = 'right'
@@ -40,12 +44,9 @@ class Picker:
 
         # Set the desired orientation for the end effector to target, will have to tune this
         # TODO
-        request.ik_request.pose_stamped.pose.position.x = 0.5
-        request.ik_request.pose_stamped.pose.position.y = 0.5
-        request.ik_request.pose_stamped.pose.position.z = 0.0
-        # request.ik_request.pose_stamped.pose.position.x = target.pose.position.x
-        # request.ik_request.pose_stamped.pose.position.y = target.pose.position.y
-        # request.ik_request.pose_stamped.pose.position.z = target.pose.position.z
+        request.ik_request.pose_stamped.pose.position.x = target.pose.position.x
+        request.ik_request.pose_stamped.pose.position.y = target.pose.position.y
+        request.ik_request.pose_stamped.pose.position.z = target.pose.position.z
         request.ik_request.pose_stamped.pose.orientation.x = 0.0
         request.ik_request.pose_stamped.pose.orientation.y = 1.0
         request.ik_request.pose_stamped.pose.orientation.z = 0.0
@@ -70,6 +71,7 @@ class Picker:
 
     # Callback
     def pickBall(self, request):
+        print("REQUEST TO PICK UP BALL")
         # get the position of the ball in ?? coordinates
         ball_pose = self.get_ball_pose()
 
@@ -81,7 +83,7 @@ class Picker:
         rospy.sleep(1.0)
 
         # use move it to compute IK and orient the end effector
-        self._moveArmToTarget(ball_pose)
+        # self._moveArmToTarget(ball_pose)
 
         # close gripper
         self.right_gripper.close()
