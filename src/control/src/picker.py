4a7dab0655032a89da9fcac588f96996f607c0e5
Osher Lerner <osherler@gmail.com>
2021-11-29 20:31:38 -0800

    hard code picker

--- src/control/src/picker.py
+++ src/control/src/picker.py
@@ -21,7 +21,8 @@ class Picker:
         rospy.Service('pick_ball', PickUpBall, self.pickBall)
 
         # Create the function used to get the position of the ball
-        self.get_ball_pose = rospy.ServiceProxy('ball_pose', GetBallPose)
+        # self.get_ball_pose = rospy.ServiceProxy('ball_pose', GetBallPose)
+        self.get_ball_pose = lambda: None
         self.compute_ik = rospy.ServiceProxy('compute_ik', GetPositionIK)
 
         self.arm = 'right'
@@ -42,6 +43,9 @@ class Picker:
         request.ik_request.pose_stamped.pose.position.x = 0.5
         request.ik_request.pose_stamped.pose.position.y = 0.5
         request.ik_request.pose_stamped.pose.position.z = 0.0
+        # request.ik_request.pose_stamped.pose.position.x = target.pose.position.x
+        # request.ik_request.pose_stamped.pose.position.y = target.pose.position.y
+        # request.ik_request.pose_stamped.pose.position.z = target.pose.position.z
         request.ik_request.pose_stamped.pose.orientation.x = 0.0
         request.ik_request.pose_stamped.pose.orientation.y = 1.0
         request.ik_request.pose_stamped.pose.orientation.z = 0.0
@@ -92,64 +96,6 @@ class Picker:
 if __name__ == '__main__':
     node = Picker()
     node.run()
-
-
-"""
-if __name__ == '__main__':
-    
-    # Waits for the ball pose service to become available
-    rospy.wait_for_service('ball_pose')
-    
-    # Initializes the node
-    rospy.init_node('picker_node')
-    
-    # Creates a function used to call the 
-    # ball pose service: PoseSrv is the service type
-    ball_pose_srv = rospy.ServiceProxy('ball_pose', PoseSrv)
-
-    # Create an empty list to hold the coordinates of the clicked points
-    points = []
-
-
-    while not rospy.is_shutdown():
-    try:
-      # Waits for a key input to continue
-      raw_input('Press enter to capture an image:')
-    except KeyboardInterrupt:
-      print 'Break from raw_input'
-      break
-    
-    try:
-      # Request the last image from the image service
-      # And extract the ROS Image from the ImageSrv service
-      # Remember that ImageSrv.image_data was
-      # defined to be of type sensor_msgs.msg.Image
-      ros_img_msg = last_image_service().image_data
-
-      # Convert the ROS message to a NumPy image
-      np_image = ros_to_np_img(ros_img_msg)
-
-      # Display the CV Image
-      cv2.imshow("CV Image", np_image)
-
-      # Tell OpenCV that it should call 'on_mouse_click' when the user
-      # clicks the window. This will add clicked points to our list
-      cv2.setMouseCallback("CV Image", on_mouse_click, param=1)
-
-      # Zero out list each time we have a new image
-      points = []
-
-      # Loop until the user has clicked enough points
-      while len(points) < TOT_CLICKS:
-        if rospy.is_shutdown():
-          raise KeyboardInterrupt
-        cv2.waitKey(10)
-
-      # Convert the Python list of points to a NumPy array of the form
-      #   | u1 u2 u3 u4 |
-      #   | v1 v2 v3 v4 |
-      uv = np.array(points).T
-"""
 
 
 
